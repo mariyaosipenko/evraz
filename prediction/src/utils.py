b7bf6a5f1972d0e56f05b469a427d3170027be28
IImbryk <3533868@gmail.com>
2023-02-19 00:34:32 +0300

    upd_code

--- prediction/src/utils.py
+++ prediction/src/utils.py
@@ -15,3 +15,18 @@ def get_amplitude(y):
 def check_df_size(df):
     return 0 if len(df) < 5 else 1
 
+
+def renaming(df):
+
+    if (df.name == 7) & (df.podtype_id == 4):
+        return 'bearing7_hor'
+
+    if (df.name == 7) & (df.podtype_id == 5):
+        return 'bearing7_vert'
+
+    if (df.name == 8) & (df.podtype_id == 4):
+        return 'bearing8_hor'
+
+    if (df.name == 8) & (df.podtype_id == 5):
+        return 'bearing8_vert'
+
