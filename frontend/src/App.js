30db4deefc8b648a4ed36689478759fec6d8718e
MentalWombat <e.ochneva@gmail.com>
2023-02-19 01:15:11 +0300

    correct url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
   useEffect(() => {
     const getData = async () => {
       try {
-        const { data } = await axios.get("/signal");
+        const { data } = await axios.get("http://127.0.0.1:8000/signal");
         console.log(data);
       } catch (error) {
         console.error("oops", error);
